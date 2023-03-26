3bb894cca738cfaba0bec5e60f5d070ca4d3d3d9
Fernando Pereira <fnd.alemao@hotmail.com>
2023-03-26 12:42:11 -0300

    Swagger on production

--- Agenda.Api/Program.cs
+++ Agenda.Api/Program.cs
@@ -77,11 +77,10 @@ builder.Services.AddSwaggerGen(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthentication();
 
